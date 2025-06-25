7a09aee1fa9f97371759e7cb8af2e06a2973a9ca
reshma1223 <chilakalapudisrireshma@gmail.com>
2025-06-25 15:38:46 +0530

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -1,85 +1,59 @@
-from PIL import Image
+clc;
+clear variables;
+% Load the image and convert to grayscale if it is RGB
+I=imread('input.png');
+[rows, columns, numberOfColorChannels] = size(I);
+if numberOfColorChannels > 1
+    I = rgb2gray(I);
+end
+L=256;
+imageWithHiddenData=I;
 
-def genData(data):
-    """Converts input text into a list of 8-bit binary strings."""
-    return [format(ord(i), '08b') for i in data]
+message=input('Please enter the message you want to hide: ','s');
+% Each character occupies a byte, so total bits can be found by multiplying
+% string length by 8
+len=strlength(message)*8;
+% error handling
+if strlength(message) > (rows*columns)
+    disp('Cover image size is not enough to encode full message!')
+end
+ascii_values=uint8(message);   
+ascii2binary=dec2bin(ascii_values,8);
 
-def modPix(pix, data):
-    """Modifies pixel values to encode the binary data."""
-    datalist = genData(data)
-    lendata = len(datalist)
-    imdata = iter(pix)
-    
-    for i in range(lendata):
-        pixels = [value for value in next(imdata)[:3] + next(imdata)[:3] + next(imdata)[:3]]
+% Append all binary equivalents of ascii values into one string
+binary_sequence='';  
+for i=1:strlength(message)
+    binary_sequence=append(binary_sequence,ascii2binary(i,:));
+end
+
+% To track how many bits of message have been hidden
+bitCount=1;
+
+for i=1:rows
+    for j=1:columns
         
-        # Modify pixel values based on binary data
-        for j in range(8):
-            if datalist[i][j] == '0' and pixels[j] % 2 != 0:
-                pixels[j] -= 1
-            elif datalist[i][j] == '1' and pixels[j] % 2 == 0:
-                pixels[j] = pixels[j] - 1 if pixels[j] != 0 else pixels[j] + 1
-        
-        # Set termination flag (last pixel even means continue, odd means stop)
-        if i == lendata - 1:
-            pixels[-1] |= 1  # Make odd (stop flag)
-        else:
-            pixels[-1] &= ~1  # Make even (continue flag)
-        
-        yield tuple(pixels[:3])
-        yield tuple(pixels[3:6])
-        yield tuple(pixels[6:9])
+        if bitCount<=len
+            %Obtain the LSB of the grey level of the pixel
+            LSB=bitget(I(i,j),1);
+            
+            %Convert the bit from the message to numeric form
+            bitInMessage=str2double(binary_sequence(bitCount));
+            %Reset LSB
+            imageWithHiddenData(i,j)= bitand(imageWithHiddenData(i,j),0b11111110);
+            %Replace LSB with bitInMessage
+            imageWithHiddenData(i,j) = imageWithHiddenData(i,j) + bitInMessage;
+            
+            bitCount=bitCount+1;
+        end
+    end
+end
 
-def encode_enc(newimg, data):
-    """Encodes the modified pixel data into the new image."""
-    w = newimg.size[0]
-    (x, y) = (0, 0)
-    
-    for pixel in modPix(newimg.getdata(), data):
-        newimg.putpixel((x, y), pixel)
-        x = 0 if x == w - 1 else x + 1
-        y += 1 if x == 0 else 0
+subplot(1,2,1);
+imshow(I);
+title('Input Image');
 
-def encode():
-    """Handles user input and calls encoding functions."""
-    img = input("Enter image name (with extension): ")
-    image = Image.open(img, 'r')
-    data = input("Enter data to be encoded: ")
-    
-    if not data:
-        raise ValueError("Data is empty")
-    
-    newimg = image.copy()
-    encode_enc(newimg, data)
-    new_img_name = input("Enter the name of new image (with extension): ")
-    newimg.save(new_img_name, new_img_name.split(".")[-1].upper())
+subplot(1,2,2);
+imshow(imageWithHiddenData);
+title('Image with Hidden Data');
 
-def decode():
-    """Decodes hidden text from an image."""
-    img = input("Enter image name (with extension): ")
-    image = Image.open(img, 'r')
-    imgdata = iter(image.getdata())
-    data = ""
-    
-    while True:
-        pixels = [value for value in next(imgdata)[:3] + next(imgdata)[:3] + next(imgdata)[:3]]
-        binstr = ''.join(['1' if i % 2 else '0' for i in pixels[:8]])
-        data += chr(int(binstr, 2))
-        
-        if pixels[-1] % 2 != 0:
-            break
-    
-    return data
-
-def main():
-    """Main function for user interaction."""
-    choice = input(":: Welcome to Steganography ::\n1. Encode\n2. Decode\n")
-    if choice == '1':
-        encode()
-    elif choice == '2':
-        print("Decoded Word: " + decode())
-    else:
-        print("Invalid choice, exiting.")
-
-if __name__ == "__main__":
-    main()
+imwrite(imageWithHiddenData,'output.png')
